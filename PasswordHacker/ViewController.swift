152c2e6d03a2b6fb12e8f9225adcc7f4bf634f1f
HARUKA ito <haruka_ito@keio.jp>
2020-09-15 01:44:55 +0900

    Initial Commit

---
+++ PasswordHacker/ViewController.swift
@@ -0,0 +1,73 @@
+//
+//  ViewController.swift
+//  PasswordHacker
+//
+//  Created by 伊藤　陽香 on 2020/09/13.
+//  Copyright © 2020 伊藤　陽香. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+    
+    @IBOutlet var resultLabel1: UILabel! //右から一番目
+    @IBOutlet var resultLabel2: UILabel!
+    @IBOutlet var resultLabel3: UILabel!
+    @IBOutlet var resultLabel4: UILabel!
+    
+    @IBOutlet var countLabel: UILabel!
+    
+    var password:Int = 1234
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+    
+    @IBAction func start() {
+        
+        for i in 0...9999 {
+            
+            //解析中の数字を表示する
+            countLabel.text = String(i)
+            
+            //処理を0.0005秒待機する
+            RunLoop.main.run(until: Date(timeIntervalSinceNow: 0.0005))
+            
+            if i == password{
+                
+                var digits = [Int]()
+                
+                for _ in 0...3{
+                    digits.append(password % 10)
+                    password = password / 10
+                }
+                
+                resultLabel1.text = String(digits[0])
+                resultLabel2.text = String(digits[1])
+                resultLabel3.text = String(digits[2])
+                resultLabel4.text = String(digits[3])
+                
+                
+                print("正解は\(i)です")
+            }
+        }
+        
+    }
+    
+    @IBAction func reset() {
+        
+        password = 1234
+        
+        countLabel.text = "STARTボタンを押して解析開始"
+        
+        resultLabel1.text = "0"
+        resultLabel2.text = "0"
+        resultLabel3.text = "0"
+        resultLabel4.text = "0"
+        
+    }
+
+
+}
+
